00ba7edba08a72e44ea45cd7815d8b221763d10f
Alexey Sapr <alexeysapr@gmail.com>
2021-11-17 16:58:24 +0200

    Add api-service

--- src/index.js
+++ src/index.js
@@ -1,47 +1,56 @@
 import './sass/main.scss';
 
+import debounce from 'lodash.debounce';
 import card from './template/card.hbs';
 import apiService from './js/api-service';
 
 const galleryList = document.querySelector('.list');
 const searchForm = document.querySelector('.search-form');
 
-searchForm.addEventListener('submit', onSubmit);
+/*******************поиск по запросу******************************* */
+const inputRef = searchForm.querySelector('input');
 
-function onSubmit(event) {
-  event.preventDefault();
+const DEBOUNCE_DELAY = 1000;
 
-  getData();
+inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+async function onInput(event) {
+  const query = event.target.value.trim();
+  if (!query) {
+    console.log('emty string');
+    getData();
+    return;
+  }
+
+  try {
+    const movies = await apiService.getMovies(query);
+    showMovies(movies.results);
+  } catch (error) {
+    console.error(error);
+  }
 }
+/******************************************************** */
+
+// searchForm.addEventListener('submit', onSubmit);
+
+// function onSubmit(event) {
+//   event.preventDefault();
+
+//   getData();
+// }
+
+getData();
 
 async function getData() {
   try {
     const movies = await apiService.getMovies();
-    const moviesArr = movies.results;
-
-    const genresList = await apiService.getGenres().then(obj => obj.genres);
-
-    moviesArr.forEach(movie => {
-      movie.genre_ids = Genres(genresList, movie.genre_ids);
-      movie.release_date = getYear(movie.release_date);
-    });
-
-    galleryList.insertAdjacentHTML('beforeend', card(moviesArr));
+    showMovies(movies.results);
   } catch (error) {
     console.error(error);
   }
 }
 
-function getYear(date) {
-  return date.slice(0, 4);
-}
-
-function Genres(genresArray, idArray) {
-  const resArr = [];
-  idArray.forEach(id => {
-    for (let i = 0; i < genresArray.length; i++) {
-      genresArray[i].id === id ? resArr.push(genresArray[i].name) : undefined;
-    }
-  });
-  return resArr.join(', ');
+function showMovies(movies) {
+  // galleryList.insertAdjacentHTML('beforeend', card(moviesArr));
+  galleryList.innerHTML = card(movies);
 }
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -10,26 +10,66 @@ const AUTH_KEY =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2ZkMjA5NzBkNzRkOWEzZjk0NjZkOGQ2YzllNjI5NyIsInN1YiI6IjYxOTNhYjExNDJmMTlmMDA0MzFlZTkzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z6zVAqAKYmCRhBa4xmofDsVBFw9-x8O5I_GnOqiw-F8';
 axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_KEY}`;
 
+async function getGenres() {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list');
+    return response.data.genres;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getYear(date) {
+  return date ? date.slice(0, 4) : undefined;
+}
+
+function Genres(genresArray, idArray) {
+  const resArr = [];
+  idArray.forEach(id => {
+    for (let i = 0; i < genresArray.length; i++) {
+      genresArray[i].id === id ? resArr.push(genresArray[i].name) : undefined;
+    }
+  });
+  return resArr.join(', ');
+}
+
 export default {
-  async getMovies() {
+  async getMovies(query) {
+    let response;
     try {
-      const response = await axios.get('https://api.themoviedb.org/3/trending/movie/week');
-      // console.log('response: ', response.data);
-      return response.data;
+      if (query) {
+        response = await axios.get(
+          `https://api.themoviedb.org/3/search/movie?language=en-US&query=${query}&page=1&include_adult=false`,
+        );
+      } else {
+        response = await axios.get('https://api.themoviedb.org/3/trending/movie/week');
+      }
+
+      const movies = await response.data;
+
+      const genresList = await getGenres();
+
+      movies.results.forEach(movie => {
+        movie.genre_ids = Genres(genresList, movie.genre_ids);
+        movie.release_date = getYear(movie.release_date);
+      });
+
+      return movies;
     } catch (error) {
       console.error(error);
     }
   },
 
-  async getGenres() {
-    try {
-      const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list');
-      // console.log('response: ', response.data);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
+  // async getMovie(query) {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://api.themoviedb.org/3/search/movie?language=en-US&query=${query}&page=1&include_adult=false`,
+  //     );
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // },
 
   incrementPage() {
     this.config.params.page += 1;

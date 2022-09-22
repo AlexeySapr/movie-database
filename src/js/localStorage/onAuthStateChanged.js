f6979139f1ab454d63f490cd05ba7467d4fed5a0
Alexey Sapr <alexeysapr@gmail.com>
2022-09-22 17:44:44 +0200

    add init movies to local storage

--- src/js/localStorage/onAuthStateChanged.js
+++ src/js/localStorage/onAuthStateChanged.js
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase/firebaseData';
 import { refs } from '../refs';
 import { onAuthInClick, onAuthOutClick } from '../authorization/authModal';
+import * as FireStore from '../firebase/fireStoreService';
 
 // add/remove current user in local storage
 onAuthStateChanged(auth, user => {
@@ -23,6 +24,24 @@ onAuthStateChanged(auth, user => {
         email: user.email,
       }),
     );
+
+    FireStore.getMovies('watchedMovies')
+      .then(moviesArr => {
+        let moviesIdArr = moviesArr.map(movie => movie.id) || [];
+        localStorage.setItem('watchedMovies', JSON.stringify(moviesIdArr));
+      })
+      .catch(err => {
+        console.log('err in onAuth: ', err.message);
+      });
+
+    FireStore.getMovies('queueMovies')
+      .then(moviesArr => {
+        let moviesIdArr = moviesArr.map(movie => movie.id) || [];
+        localStorage.setItem('queueMovies', JSON.stringify(moviesIdArr));
+      })
+      .catch(err => {
+        console.log('err in onAuth: ', err.message);
+      });
   } else {
     if (authBtnRefs.classList.contains('signOut')) {
       authBtnRefs.classList.remove('signOut');
@@ -34,7 +53,7 @@ onAuthStateChanged(auth, user => {
     authBtnRefs.addEventListener('click', onAuthInClick);
 
     localStorage.removeItem('user');
-    // localStorage.removeItem('watchedMovies');
-    // localStorage.removeItem('queueMovies');
+    localStorage.removeItem('watchedMovies');
+    localStorage.removeItem('queueMovies');
   }
 });
